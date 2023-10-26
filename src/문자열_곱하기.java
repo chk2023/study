dd29cc47ab6650d77f79a6189fefd008d77f3987
CHO HYONGKI <120306336+chk2023@users.noreply.github.com>
2023-10-26 12:11:07 +0900

    prgrammers lv0

---
+++ src/문자열_곱하기.java
@@ -0,0 +1,9 @@
+public class 문자열_곱하기 {
+    public String solution(String my_string, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < k; i++) {
+            sb.append(my_string);
+        }
+        return sb.toString();
+    }
+}
