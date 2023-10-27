73fe51f77c4b53122b2baed8490d5e081aaec462
CHO HYONGKI <120306336+chk2023@users.noreply.github.com>
2023-10-27 10:40:43 +0900

    prgrammers lv0

---
+++ src/공배수.java
@@ -0,0 +1,9 @@
+public class 공배수 {
+    public int solution(int number, int n, int m) {
+        if ((number%n==0) && (number%m==0)){
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
