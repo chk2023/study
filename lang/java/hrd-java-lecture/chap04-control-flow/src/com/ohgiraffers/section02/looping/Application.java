d3b8f392827ca0801bf8fc9a0c973157fbff3f49
CHO HYONGKI <120306336+chk2023@users.noreply.github.com>
2024-01-04 09:15:52 +0900

    Add files via upload

---
+++ lang/java/hrd-java-lecture/chap04-control-flow/src/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application {
+    public static void main(String[] args) {
+        A_for a = new A_for();
+//        a.testSimpleForStatement();
+//        a.testForExample4();
+//        a.printSimpleGugudan();
+
+        A_nestedFor a2 = new A_nestedFor();
+//        a2.printGugudanFromTwoToNine();
+//        a2.printStarInputRowTimes();
+        a2.printTriangleStars();
+    }
+}
