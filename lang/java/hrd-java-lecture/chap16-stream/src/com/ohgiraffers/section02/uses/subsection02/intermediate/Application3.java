97ed1fca78e2fbe762850c2cff62d941dac23743
chk2023 <jidanmian777@gmail.com>
2024-01-22 14:07:21 +0900

    study

--- lang/java/hrd-java-lecture/chap16-stream/src/com/ohgiraffers/section02/uses/subsection02/intermediate/Application3.java
+++ lang/java/hrd-java-lecture/chap16-stream/src/com/ohgiraffers/section02/uses/subsection02/intermediate/Application3.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 
 public class Application3 {
     public static void main(String[] args) {
-        /* 스트림의 중계 연산 중 하나인 flatMap에 대해 이해하고 사용할 수 있다. */
+        /* 스트림의 중계 연산 중 하나인 flatMap에 대해 이해하고 사용할 수 있다.
+        * 중첩 구조를 한 단계 제거하고 단일 컬렉션으로 만들어 준다. => 플래트닝(flattening)
+        * */
 
         List<List<String>> list = Arrays.asList(
                 Arrays.asList("JAVA", "SPRING", "SPRINGBOOT"),
